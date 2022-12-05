57f0d18d178da61a87e74a14af8ab4a26a71bca5
Tugdual <harel.tugdual@gmail.com>
2022-12-06 00:19:37 +0100

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+// Inclure les librairies nécessaires
+var marked = require('marked');
+var GoogleDriveAPI = require('google-drive-api');
+
+// Initialiser l'éditeur de markdown et le rendu HTML
+var editor = document.getElementById('editor');
+var preview = document.getElementById('preview');
+
+// Mettre à jour le rendu HTML lorsque le contenu de l'éditeur est modifié
+editor.addEventListener('input', function() {
+    preview.innerHTML = marked(editor.value);
+});
+
+// Initialiser l'API Google Drive
+GoogleDriveAPI.init({
+    clientId: 'YOUR_CLIENT_ID',
+    scope: 'https://www.googleapis.com/auth/drive'
+});
+
+// Gérer la sauvegarde des notes dans Google Drive
+document.getElementById('save').addEventListener('click', function() {
+    GoogleDriveAPI.saveFile({
+        content,
+        name: 'notes.md' // Nom du fichier
+    });
+});
